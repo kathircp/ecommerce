7cb3ce6996237ef7f99bea8cd047fc40d1e36b53
Bala <Bala@DESKTOP-36C47SP>
2025-11-01 17:04:59 +0530

    removed browser module

--- FrontEnd/ecommerce/src/app/app.module.ts
+++ FrontEnd/ecommerce/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTreeModule } from '@angular/material/tree';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
